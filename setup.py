5e41f0725a285df21a3ad6fa4140bac18ccfd30e
cookkkie <cookie.io@openmailbox.org>
2017-06-26 02:55:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='modus',
+    author='cookkkie',
+    url='https://github.com/cookkkie/modus',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='',
+    include_package_data=True,
+    install_requires=requirements
+)
