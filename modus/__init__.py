fd5f2a5555b0be6ec07e14801113310205421028
cookkkie <cookie.io@openmailbox.org>
2018-01-16 10:56:17 +0100

    Bump version

--- modus/__init__.py
+++ modus/__init__.py
@@ -1,3 +1,3 @@
 from modus.model import Model
 
-__version__ = '0.0.24'
+__version__ = '0.0.25'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='modus',
     author='cookkkie',
     url='https://github.com/cookkkie/modus',
-    version='0.0.24',
+    version='0.0.25',
     packages=find_packages(),
     license='MIT',
     description='',
