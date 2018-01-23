85fa4e30c268e93b67beb38d4ab0529767f1856c
cookkkie <cookie.io@openmailbox.org>
2018-01-23 14:18:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='modus',
     author='cookkkie',
     url='https://github.com/cookkkie/modus',
-    version='0.0.28',
+    version='0.0.29',
     packages=find_packages(),
     license='MIT',
     description='',
